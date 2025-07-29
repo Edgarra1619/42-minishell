c75aefcc9aedd7e14278236eba15dd06c5acdab3
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-07-29 14:54:39 +0100

    Add env variable functions get_var and set_var
    For general env variable handling by builtins and otherwise.

--- include/minishell.h
+++ include/minishell.h
@@ -8,5 +8,7 @@ int		echo(char **argv);
 int		pwd(void);
 int		env(void);
 void	get_env(int **varc, char **vars, char ***envp);
+char	*get_var(const char *var);
+int		set_var(const char *var);
 
 #endif
--- src/env.c
+++ src/env.c
@@ -1,7 +1,8 @@
 #include <minishell.h>
 #include <libft.h>
 
-void	get_env(int **varc, char **vars, char ***envp);
+void		get_env(int **varc, char **vars, char ***envp);
+static int	get_key_len(const char *const var);
 
 int	env(void)
 {
@@ -25,4 +26,52 @@ void	get_env(int **varc, char **vars, char ***envp)
 		*vars = (char *)s_vars;
 	if (envp)
 		*envp = s_envp;
+}
+
+char	*get_var(const char *const var)
+{
+	char		**envp;
+	const int	key_len = get_key_len(var);
+
+	get_env(NULL, NULL, &envp);
+	while (*envp)
+	{
+		if (!ft_strncmp(*envp, var, key_len) && (*envp)[key_len] == '=')
+			return (*envp + key_len + 1);
+		envp++;
+	}
+	return (NULL);
+}
+
+int	set_var(const char *const var)
+{
+	int			*varc;
+	char		*varv;
+	char		**envp;
+	const int	key_len = get_key_len(var);
+
+	get_env(&varc, &varv, &envp);
+	if (*varc >= ENV_MAX || !ft_strchr(var, '='))
+		return (1);
+	while (*envp && ft_strncmp(*envp, var, key_len + 1))
+		++envp;
+	if (!*envp)
+	{
+		while (*varv)
+			varv += VAR_MAX;
+		*envp = varv;
+		++*varc;
+	}
+	ft_strlcpy(*envp, var, VAR_MAX);
+	return (0);
+}
+
+static int	get_key_len(const char *const var)
+{
+	int	i;
+
+	i = 0;
+	while (var[i] && var[i] != '=')
+		i++;
+	return (i);
 }
