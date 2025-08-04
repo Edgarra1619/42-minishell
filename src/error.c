54a9d147447c0e2f8f1bc07068423d464825643f
Edgar Amorim <edgribei@student.42porto.com>
2025-08-04 17:21:12 +0100

    Add buffer system to print_error Now, the function only does one write call

--- src/error.c
+++ src/error.c
@@ -1,30 +1,50 @@
 #include <libft.h>
+
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+
+static void	buffer_error(const char *str);
 
 int	print_error(const char *const command, const char *const path,
 	const char *const var, const char *const error)
 {
-	ft_putstr_fd("minishell: ", 2);
+	buffer_error("minishell: ");
 	if (command)
 	{
-		ft_putstr_fd(command, 2);
-		ft_putstr_fd(": ", 2);
+		buffer_error(command);
+		buffer_error(": ");
 	}
 	if (path)
 	{
-		ft_putstr_fd(path, 2);
-		ft_putstr_fd(": ", 2);
+		buffer_error(path);
+		buffer_error(": ");
 	}
 	if (var)
 	{
-		ft_putstr_fd("`", 2);
-		ft_putstr_fd(var, 2);
-		ft_putstr_fd("': ", 2);
+		buffer_error("'");
+		buffer_error(var);
+		buffer_error("': ");
 	}
 	if (error)
-		ft_putendl_fd(error, 2);
+		buffer_error(error);
 	else
-		ft_putendl_fd(strerror(errno), 2);
+		buffer_error(strerror(errno));
+	buffer_error("\n");
+	buffer_error(NULL);
 	return (1);
+}
+
+//flushes on NULL
+static void	buffer_error(const char *str)
+{
+	static char	buffer[1024];
+
+	if (!str)
+	{
+		write(2, buffer, ft_strlen(buffer));
+		buffer[0] = 0;
+	}
+	else
+		ft_strlcat(buffer, str, 1024);
 }
