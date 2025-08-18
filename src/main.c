fc53a1b69ad6637e118840a55b9e62395cc10907
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-08-18 13:41:29 +0100

    Add initial main
    Simple prompt loop with working pipeline.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,35 @@
+#include <minishell/env.h>
+#include <minishell/exec.h>
+#include <minishell/tokenizer.h>
+#include <libft.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
 int	main(int argc, char **argv, char **prev_envp)
 {
+	t_pipeline	pl;
+	char		*status;
+
+	(void)argc;
+	(void)argv;
+	init_env(prev_envp);
+	get_env(NULL, NULL, NULL, &status);
+	while (1)
+	{
+		pl.prompt = readline("$ ");
+		if (!pl.prompt)
+			return (1);
+		add_history(pl.prompt);
+		if (tokenize_pipeline(&pl))
+		{
+			free(pl.prompt);
+			continue ;
+		}
+		exec_pipeline(&pl);
+		wait_pipeline(&pl);
+		free(pl.prompt);
+	}
+	return (0);
 }
