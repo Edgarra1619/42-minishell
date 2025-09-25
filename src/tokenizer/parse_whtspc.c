78dda372259edb1dba8f83ce1d2e2aff3c3ffc18
Edgar Amorim <edgribei@student.42porto.com>
2025-09-25 15:00:48 +0100

    Fix error handling in parse_whtspc

--- src/tokenizer/parse_whtspc.c
+++ src/tokenizer/parse_whtspc.c
@@ -27,13 +27,7 @@ int	parse_whtspc(int *argc, char ***arg, char **str)
 	new = ft_realloc(*arg, sizeof(char *) * (*argc + 1),
 			sizeof(char*) * (*argc + 2));
 	if (!new)
-	{
-		new = *arg;
-		while (*new)
-			free((*new)++);
-		free(*arg);
 		clear_exit(1);
-	}
 	*arg = new;
 	(*arg)[*argc + 1] = NULL;
 	return (0);
