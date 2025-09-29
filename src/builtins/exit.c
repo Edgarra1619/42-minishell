5635ad3cd6f45e4e3e4271887ab6689992d1bcf1
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-09-29 11:56:55 +0100

    Fix exit "numeric argument required" error
    - Fix numeric argument verification;
    - Print invalid argument's value;
    - Return status 2;

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -15,6 +15,8 @@
 #include <minishell/exit.h>
 #include <libft.h>
 
+static bool is_num(const char *str);
+
 int	exit_builtin(char **argv, const bool print_output, const bool is_single_cmd)
 {
 	char	*env_status;
@@ -31,14 +33,30 @@ int	exit_builtin(char **argv, const bool print_output, const bool is_single_cmd)
 			print_error("exit", NULL, "too many arguments");
 		return (1);
 	}
-	else if (ft_atoi_safe(&status, argv[1]))
+	else if (!is_num(argv[1]) || ft_atoi_safe(&status, argv[1]))
 	{
 		if (print_output)
-			print_error("exit", NULL, "numeric argument required");
-		return (1);
+			print_error("exit", argv[1], "numeric argument required");
+		return (2);
 	}
 	if (is_single_cmd && print_output)
 		ft_putstr_fd("exit\n", 2);
 	clear_exit(status);
 	return (1);
+}
+
+static bool is_num(const char *str)
+{
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (true)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+		if (!*str)
+			return (true);
+	}
 }
